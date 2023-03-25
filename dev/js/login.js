88f4530a8031cce2f7a1c72d78ea153b015a363f
suply_company <motagenao0@gmail.com>
2023-03-25 16:34:01 -0400

    nueva interface
    al inicial la pag y en la app web ""

---
+++ dev/js/login.js
@@ -0,0 +1,12 @@
+
+let username = document.getElementById("user");
+let password = document.getElementById("password");
+let buton = document.getElementById("button");
+
+
+function entrar(){
+    location.href="app.html";
+    console.log(username);
+    console.log(password);
+    
+}
---
+++ dev/js/registro-app.js
@@ -0,0 +1,16 @@
+
+let username = document.getElementById("user");
+let password = document.getElementById("password");
+let buton = document.getElementById("button");
+
+
+function entrar(){
+    location.href="app.html";
+    console.log(username);
+    console.log(password);
+    
+}
+function aplicacion (){
+    let 
+}
+
