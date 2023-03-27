f38e0a2956ec2197282c4f51eba2d0be67e04ae5
suply_company <motagenao0@gmail.com>
2023-03-27 02:04:37 -0400

    agregue informacion a la bara de usuario
    user interactive complete

--- dev/js/login.js
+++ dev/js/login.js
@@ -1,8 +1,9 @@
 function entrar(){
+    location.href="apk-web.html";
     let username = document.getElementById("user").value;
     let password = document.getElementById("password").value;
     localStorage.setItem("name", username )
     localStorage.setItem("nickname", password )
-    location.href="apk-web.html";
+    
     
 }
